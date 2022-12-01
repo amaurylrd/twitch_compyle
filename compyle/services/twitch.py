cf9d235aa0ea7d5ba5ad773428352bb6e5a2a1b3
amaurylrd <amaurylrd@yahoo.fr>
2022-12-01 18:59:05 +0100

    presque une v1

--- compyle/services/twitch.py
+++ compyle/services/twitch.py
@@ -1,10 +1,12 @@
-# pylint: disable=unused-argument
+# pylint: disable=unused-argument, unnecessary-lambda-assignment
 
+import datetime
 from os import getenv
 from typing import Any
-import datetime
+
 from requests import HTTPError
-from routing import Endpoint, Routable
+
+from compyle.services.routing import Endpoint, Routable
 
 
 class TwitchApi(Routable):
@@ -12,9 +14,13 @@ class TwitchApi(Routable):
         helix_url = "https://api.twitch.tv/helix"
         oauth2_url = "https://id.twitch.tv/oauth2"
 
+        # TODO https://dev.twitch.tv/docs/api/guide healthz + chaque endpoint
+
         self.router.register("auth", Endpoint(oauth2_url, "/token", req=["client_id", "client_secret", "grant_type"]))
-        self.router.register("validate", Endpoint(helix_url, "/validate"))
+        self.router.register("validate", Endpoint(oauth2_url, "/validate"))
+
         self.router.register("games", Endpoint(helix_url, "/games/top", opt=["after", "before", "first"]))
+        self.router.register("game", Endpoint(helix_url, "/games", req=["name"]))
         self.router.register(
             "clips",
             Endpoint(helix_url, "/clips", req=["game_id"], opt=["started_at", "ended_at", "before", "first", "after"]),
@@ -22,7 +28,20 @@ class TwitchApi(Routable):
 
         self.client_id = getenv("TWITCH_APP_CLIENT_ID")
         self.client_secret = getenv("TWITCH_APP_CLIENT_SECRET")
-        self.access_token = self.get_new_access_token()
+        self.access_token = self.get_new_access_token()["access_token"]
+
+    def __request_header(self, *, client_id=True, acces_token=True):
+        header = {
+            "Accept": "application/vnd.twitchtv.v5+json",
+        }
+
+        if client_id:
+            header["Client-ID"] = self.client_id
+
+        if acces_token and self.access_token:
+            header["Authorization"] = f"Bearer {self.access_token}"
+
+        return header
 
     def get_new_access_token(self) -> Any:
         """Gets a new client access token from Twitch API.
@@ -34,8 +53,8 @@ class TwitchApi(Routable):
         Returns:
             Any: the response deserialized as JSON if the request was successful, raises an error otherwise.
         """
-        params = {"client_id": self.client_id, "client_secret": self.client_secret, "grant_type": "client_credentials"}
         header = {"Content-Type": "application/x-www-form-urlencoded", "Accept": "application/vnd.twitchtv.v5+json"}
+        params = {"client_id": self.client_id, "client_secret": self.client_secret, "grant_type": "client_credentials"}
 
         return self.router.request("POST", "auth", header, **params)
 
@@ -51,10 +70,8 @@ class TwitchApi(Routable):
         Returns:
             bool: `True` if the token is valid, `False` otherwise.
         """
-        header = {"Authorization": f"OAuth {access_token}"}
-
         try:
-            self.router.request("GET", "validate", header)
+            self.router.request("GET", "validate", {"Authorization": f"OAuth {access_token}"})
         except HTTPError:
             return False
         return True
@@ -64,7 +81,7 @@ class TwitchApi(Routable):
 
         Example:
             >>> self.get_top_games(limit=1)
-            >>> [{"id": "33214", "name": "Fortnite", "box_art_url": "..."}]
+            >>> {{"id": "33214", "name": "Fortnite", "box_art_url": "...", ...}}
 
         Args:
             limit (int, optional): the size of a page. Defaults to 10.
@@ -72,57 +89,68 @@ class TwitchApi(Routable):
         Returns:
             Any: the list of top viewed games, raises an error otherwise.
         """
-        params = {"first": limit}
-        header = {"Client-ID": self.client_id, "Authorization": f"Bearer {self.access_token}"}
+        params = {"first": max(1, min(limit, 100))}
 
-        return self.router.request("GET", "games", header, **params)
+        return self.router.request("GET", "games", self.__request_header(), **params)
 
-    def get_games_clips(self, game_id, *, limit=100, period=7) -> Any:
-        started_at = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=period)
-        ended_at = datetime.datetime.now(datetime.timezone.utc)
+    def get_game_id(self, game_name):
+        params = {"name": game_name}
 
+        response = self.router.request("GET", "game", self.__request_header(), **params)
+
+        return response["data"][0]["id"]
+
+    def get_game_clips(self, game_id, *, limit=100, period=3) -> Any:
+        started_at = datetime.datetime.utcnow() - datetime.timedelta(days=period)
+        ended_at = datetime.datetime.utcnow()
+
+        header = self.__request_header()
         params = {
             "game_id": game_id,
-            "first": limit,
-            "started_at": started_at.astimezone().isoformat(),
-            "ended_at": ended_at.astimezone().isoformat(),
-        }
-        headers = {
-            "Accept": "application/vnd.twitchtv.v5+json",
-            "Client-ID": self.client_id,
-            "Authorization": f"Bearer {self.access_token}",
+            "first": max(1, min(limit, 100)),
+            "started_at": started_at.isoformat("T") + "Z",
+            "ended_at": ended_at.isoformat("T") + "Z",
         }
 
-        response = self.router.request("GET", "clips", headers, **params)
-        result = response["data"]
+        response = self.router.request("GET", "clips", header, **params)
 
-        while response["data"] and response["pagination"]:
+        _filter = lambda clip: clip["language"] == "fr" and clip["duration"] < 40
+        result = list(filter(_filter, response["data"]))
+        page = 0
+
+        minimum_views = 50
+        maximum_page = 10
+        minimum_clips = 20
+
+        while response["data"] and response["pagination"] and page < maximum_page:
+            if response["data"][0]["view_count"] < minimum_views or len(result) > minimum_clips:
+                break
+
             params["after"] = response["pagination"]["cursor"]
-            response = self.router.request("GET", "clips", headers, **params)
-            result += response["data"]
+            page += 1
+
+            response = self.router.request("GET", "clips", header, **params)
+
+            # TODO pas du même moment
+
+            result += list(filter(_filter, response["data"]))
+
+        result.sort(key=lambda clip: (clip["view_count"], clip["created_at"]), reverse=True)
+
+        # TODO duration moyenne, meilleur vue, whitelist
 
         return result
 
+    def get_clip_url(self, clip: Any) -> str:
+        """Gets the URL of the specified clip.
 
-from compyle.preloader import launch_after_preload
+        Args:
+            clip (Any): the JSON object representing the clip.
 
+        Returns:
+            str: the URL of the clip.
+        """
+        thumbnai_url: str = clip["thumbnail_url"]
+        index = thumbnai_url.index("-preview-")
 
-def test():
-    api = TwitchApi()
-    # print("test1", api.get_new_access_token())
-    print("test", api.get_top_games())
-
-
-launch_after_preload(test)
-
-# if "data" in serialized:
-#                 result: list = serialized["data"]
-
-#                 while serialized["data"] and serialized["pagination"] is not None:
-#                     kwargs["after"] = serialized["pagination"]["cursor"]
-
-#                     url: str = router.route(namespace, *args, **kwargs)
-#                     response: Response = HttpMethod[method](url, headers=header, timeout=None)
-
-#                     serialized = response.json()
-#                     result.extend(serialized["data"])
+        return thumbnai_url[:index] + ".mp4"
--- main.py
+++ main.py
@@ -1,91 +1,61 @@
-from compyle.api.twitch import TwitchAPI
-from compyle.preloader import launch_after_preload
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, concatenate_videoclips
+from moviepy.audio.fx.audio_normalize import audio_normalize
+from moviepy.video.compositing import transitions as transfx
 
-import os
-import os.path
-import random
-import shutil
-from moviepy.editor import *
-import requests
-import urllib.request
+from compyle.preloader import launch_after_preload
+from compyle.services.twitch import TwitchApi
+from urllib.request import urlretrieve
 
 
 def main():
-    twitch_api = TwitchAPI()
-    games = twitch_api.get_top_games()
+    api = TwitchApi()
 
-    dir_path = "./tmp/"
-    if os.path.exists(dir_path):
-        shutil.rmtree(dir_path)
-    os.mkdir(dir_path)
+    game_name = "League of Legends"
+    game_name = "VALORANT"
+    game_id = api.get_game_id(game_name)
+    clips = api.get_game_clips(game_id)
 
-    streamers = []
-    for game in games:
-        if game["name"] != "League of Legends":
-            continue
+    # import random
 
-        clips = twitch_api.get_games_clips(game["id"])
+    # random.shuffle(clips)
 
-        # clips.sort(key=lambda clip: clip["created_at"], reverse=True)
+    video = None
+    description = ""
+    subclips = []
+    subclips_duration = 0
 
-        i = 0
-        for clip in clips:
-            if clip["language"] == "fr":
-                streamers.append(clip["broadcaster_name"])
-            continue
-            # print(clip["title"], clip["created_at"], clip["view_count"])
+    for clip in clips[:10]:
+        url = api.get_clip_url(clip)
+        remote_path, _ = urlretrieve(url)
 
-            # if i >= 1:
-            #     break
+        videoclip: VideoFileClip = VideoFileClip(remote_path)
+        videoclip = audio_normalize(videoclip)
+        videoclip = videoclip.resize((1920, 1080))
+        videoclip = videoclip.set_fps(60)
 
-            if clip["language"] not in ["fr"] or clip["duration"] > 25:
-                continue
+        textclip: TextClip = TextClip(clip["broadcaster_name"], fontsize=60, color="white")
+        textclip = textclip.set_duration(videoclip.duration)
+        textclip = textclip.set_position(("left", "top"))
+        textclip = textclip.fx(transfx.slide_in, duration=1, side="left")
+        # TODO couleur
 
-            url = clip["url"]
-            i += 1
-            thumbnail_url = clip["thumbnail_url"]
-            thumbnail = thumbnail_url.index("-preview-")
-            url = thumbnail_url[:thumbnail] + ".mp4"
+        composite = CompositeVideoClip([videoclip, textclip])
+        subclips.append(composite)
 
-            print(url)
+        minutes = int(subclips_duration / 60)
+        seconds = int(subclips_duration % 60)
+        timestamp = f"{minutes:02d}:{seconds:02d}"
+        description += f"{timestamp} {clip['broadcaster_name']}\n"
+        subclips_duration += videoclip.duration
 
-            # reporthook=get_progress
-            #         def get_progress(count, block_size, total_size) -> None:
-            # percent = int(count * block_size * 100 / total_size)
-            # print(f"Downloading clip... {percent}%", end="\r", flush=True)
-            # regex = re.compile("[^a-zA-Z0-9_]")
-            file_path = dir_path + clip["title"] + ".mp4"
-            urllib.request.urlretrieve(url, file_path)
+        videoclip.reader.close()
 
-    videoclips = []
-    for file in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, file)
+    if subclips:
+        video: CompositeVideoClip = concatenate_videoclips(subclips)
+        video.write_videofile("clips.mp4", codec="libx264", verbose=False)
+        video.close()
 
-        if os.path.isfile(file_path) and file.endswith(".mp4"):
-            videoclip = VideoFileClip(file_path).fx(afx.audio_normalize)
-
-            textclip: TextClip = TextClip(txt=file, color="white", fontsize=30)
-            textclip: TextClip = textclip.set_duration(videoclip.duration)
-            textclip: TextClip = textclip.set_position(("left", "top"))
-            textclip = textclip.on_color(color=(145, 70, 255))
-            # .set_start(0.5)
-            compositeclip = CompositeVideoClip([videoclip, textclip])
-            print(compositeclip, compositeclip.duration)
-            videoclips.append(compositeclip)
-
-    print(len(clips), streamers)
-
-    if videoclips:
-        # random.shuffle(audioclips)
-
-        videoclip = concatenate_videoclips(videoclips)
-        videoclip.write_videofile(
-            "all_clips.mp4",
-            fps=24,
-            codec="libx264",
-        )
-        print("done")
-    print("pas done")
+    # pas deux fois le meme streamer à la suite ?
 
 
 if __name__ == "__main__":
